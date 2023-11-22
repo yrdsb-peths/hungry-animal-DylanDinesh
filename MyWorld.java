36727f8357ee485529c07eee2c4cc2625982438c
DylanDinesh <350444048@gapps.yrdsb.ca>
2023-11-22 10:54:33 -0500

    Updated File To Tutorial Nov 22

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World our here lives in. 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Dylan Dinesh 
+ * @version November 2023
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,21 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+
+        createApple();
+    }
+
+    /**
+     * Create a new apple at a random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
