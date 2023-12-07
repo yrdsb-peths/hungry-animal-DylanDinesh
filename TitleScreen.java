3b424fa55c96e08fd0e2842a1d7ce4b69de622c5
DylanDinesh <350444048@gapps.yrdsb.ca>
2023-12-06 21:29:49 -0500

    Added Saved Worlds
    Title Screen now has more instructions when it starts.

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2-110, 50);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -32,5 +33,20 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,102,346);
+        Label label2 = new Label("Use <-- and --> to Move", 40);
+        addObject(label2,145,303);
+        label2.setLocation(234,305);
+        Elephant elephant = new Elephant();
+        addObject(elephant,505,59);
     }
 }
